9eb1db938265bd85adca2f0e342c46cea5c939f0
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-06-29 12:13:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> arr = new List<int> { 1, 2, 3, 4 };
+var sublist = new List<List<int>>();
+
+for (int length = 1; length < arr.Count; length++)
+{
+    for (int j = 0; j < arr.Count; j++)//Starting point
+    {
+        if (j + length <= arr.Count)
+        {
+            var res = arr.GetRange(j, length);
+            sublist.Add(res);
+        }
+    }
+}
+
+foreach (var item in sublist)
+{
+    foreach (var sub in item)
+    {
+        Console.Write($"{sub} ");
+    }
+    Console.WriteLine();
+}
+
